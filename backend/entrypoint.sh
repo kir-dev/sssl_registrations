4059e21cc764d5537c3be5464b97acf55933b6f5
Beni <bucsy.benjamin@simonyi.bme.hu>
2022-03-04 13:42:29 +0100

    Fix pid path in docker entrypoint (#16)

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f /sssl_reg/tmp/pids/server.pid
 
 # If the database exists, migrate. Otherwise setup (create and migrate)
 echo "Running database migrations..."
