905321a465993922fc2f9f368d852e5ad614e4c9
rolandandai <r.andai@outlook.com>
2023-02-21 09:30:50 +0100

    trying to fix invalid authenticity token

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
+  protect_from_forgery with: :exception
 
   protected
 
--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :authsch, ENV.fetch('CLIENT_ID'), ENV.fetch('CLIENT_SECRET'), scope: 'basic mail displayName sn givenName eduPersonEntitlement'
+  provider :authsch, ENV.fetch('CLIENT_ID'), ENV.fetch('CLIENT_SECRET'), scope: 'basic mail displayName sn givenName eduPersonEntitlement', :provider_ignores_state => true
 end
