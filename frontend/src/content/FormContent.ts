7a795d9e15900506a465c2261d6bd2d7c0e4b0a8
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-03-01 10:12:52 +0100

    strings

--- frontend/src/content/FormContent.ts
+++ frontend/src/content/FormContent.ts
@@ -1,11 +1,8 @@
 export const Requirements = [
-  'Részvétel a március 3-án 18:00-tól megrendezésre kerülő megnyitó alkalmon.',
-  'Alkalmakon való részvétel hetente egyszer, a március 7. - május 1. közötti időszakban (összesen 6 alkalom).',
-  `A képzésnek van két hétvégi eseménye, ezek időpontja  április 1. - 3. és április 29. - május 1. Április 3-án
-  a program már 13:00 körül véget ér, úgyhogy ha szeretnél szavazni, akkor ennek megfelelően igazítsd a napod!`,
-  'A képzésnek több rendezvénye is van, ezért érvényes védettségi igazolvány kötelező',
-  `A jelentkezés véglegesítéséhez szükséges egy pár (kb. 10) mondatban összefoglalni és papírra
-  vetni, hogy mi a motivációd a képzésre. Ezt a motivációs levelet a nevelő tanáraitoknak adhatjátok
-  le a Schönherz kollégiumban saját színetek szintjein a határidőig (fehér - 1012, piros - 912, fekete
-  - 812, kék - 712, sárga - 612, külsős - bármelyik előző).`
+  'Részvétel a március 9-én 18:00-tól megrendezésre kerülő megnyitó alkalmon.',
+  'Alkalmakon való részvétel hetente egyszer, a március 13. - áprlisi 27. közötti időszakban (összesen 6 alkalom).',
+  'A képzésnek van két hétvégi eseménye, ezek időpontja  március 24. - 26. és április 28. - április 30',
+  `A jelentkezés véglegesítéséhez szükséges egy pár (kb. 10) mondatban összefoglalni és papírra vetni, hogy mi a motivációda képzésre.` +
+    `Ezt a motivációs levelet a nevelő tanáraitoknak adhatjátok le a Schönherz kollégiumban saját színetek szintjein a határidőig` +
+    `(fehér - 1012, piros - 912, fekete - 812, kék - 712, sárga - 612, külsős - bármelyik előző).`
 ]
